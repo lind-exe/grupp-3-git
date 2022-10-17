4055b3e8c8a334daf1ad58686ad9bb32e2145fbc
lind-exe <jojje_lind@live.se>
2022-10-17 15:15:31 +0200

    lagt till hej - jonathan

---
+++ grupp3git/grupp3git/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace grupp3git
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("Hej");
+        }
+    }
+}
